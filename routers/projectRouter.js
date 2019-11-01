2de6d852a4e9f2c240ff412db38d765f9f59a90a
Emmanuel Tanifum <etantanifum@gmail.com>
2019-11-01 12:34:00 -0400

    finised get and post request

---
+++ routers/projectRouter.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const projectModel = require("../data/helpers/projectModel.js");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  projectModel.get().then(projects => {
+    res.send(projects);
+  }).catch(() => {
+      res
+        .status(500)
+        .json({ error: "The projects information could not be retrieved." });
+  }
+
+  );
+});
+
+router.post("/", (req, res) => {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({message:"missing name or description"})
+    } else {
+        projectModel
+          .insert(req.body)
+          .then(res.status(201).json(req.body))
+          .catch({
+            error: "There was an error while saving the project to the database"
+          });
+    }
+})
+
+
+module.exports = router;
